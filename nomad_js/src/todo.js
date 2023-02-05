bc45bf5fa5c464d7833418cde37e4cf09ad9946a
Junho <lastcom1@gmail.com>
2023-02-06 02:10:48 +0900

    CSS빼고 완료

--- nomad_js/src/todo.js
+++ nomad_js/src/todo.js
@@ -3,10 +3,51 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.getElementById("todo-list");
 const todoInput = todoForm.querySelector("input");
 
+let todos = [];
+
+function saveTodos(){
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
+}
+
+function paintTodo(newTodo){
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li );
+}
+
 function onTodoSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    const newTodoObj ={
+        text : newTodo,
+        id : Date.now(),
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit",onTodoSubmit);
+
+const savedTodos = localStorage.getItem("todos");
+
+if (savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
---
+++ nomad_js/src/weather.js
@@ -0,0 +1,21 @@
+
+const API_KEY = "5fd9aa3a389ac7df3326e680f25e8e26";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
