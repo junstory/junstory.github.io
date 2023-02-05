f74c79a23febd35bc6fd0ea4fb0128b0df79cc84
Junho <lastcom1@gmail.com>
2023-02-06 02:23:42 +0900

    CSS Fin

--- nomad_js/src/weather.js
+++ nomad_js/src/weather.js
@@ -7,10 +7,10 @@ function onGeoOk(position){
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     console.log(url);
     fetch(url).then(response => response.json()).then(data =>{
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = data.weather[0].main;
+        const weather = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:first-child");
+        city.innerText = `현재 ${data.name} 날씨는`;
+        weather.innerText = `${data.weather[0].main}입니다.`;
     });
 }
 
